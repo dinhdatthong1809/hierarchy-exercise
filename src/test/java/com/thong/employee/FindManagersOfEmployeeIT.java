fa338c9828c21774c6bc3beaf75796f0e3507d29
datthong180997@gmail.com <Dinhdatthong1809>
2022-11-16 13:07:07 +0700

    Add IT for covering testFindRootManagerIds method

--- src/test/java/com/thong/employee/FindManagersOfEmployeeIT.java
+++ src/test/java/com/thong/employee/FindManagersOfEmployeeIT.java
@@ -44,4 +44,42 @@ class FindManagersOfEmployeeIT {
         Assertions.assertIterableEquals(expectedManagers, actualManagers);
     }
 
+    private static Stream<Arguments> testFindRootManagerIds() {
+        List<Employee> employees1 = List.of(
+                new Employee("Pete", "Nick"),
+                new Employee("Barbara", "Nick"),
+                new Employee("Nick", "Sophie1"),
+                new Employee("Sophie", "Jonas")
+        );
+
+        List<Employee> employees2 = List.of(
+                new Employee("Pete", "Nick"),
+                new Employee("Sophie", "Jonas")
+        );
+
+        List<Employee> employees3 = List.of(
+                new Employee("Pete", "Nick")
+        );
+
+        List<Employee> employees4 = List.of(
+                new Employee("A", "B"),
+                new Employee("C", "D"),
+                new Employee("E", "F")
+        );
+
+        return Stream.of(
+                Arguments.of(employees1, List.of("Sophie1", "Jonas")),
+                Arguments.of(employees2, List.of("Nick", "Jonas")),
+                Arguments.of(employees3, List.of("Nick")),
+                Arguments.of(employees4, List.of("B", "D", "F"))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("testFindRootManagerIds")
+    void testFindRootManagerIds(List<Employee> employees, List<String> expectedRootManagerIds) {
+        List<String> actualRootManagerIds = employeeService.findRootManagerIds(employees);
+        Assertions.assertIterableEquals(expectedRootManagerIds, actualRootManagerIds);
+    }
+
 }
